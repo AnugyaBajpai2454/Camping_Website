4d7e5e33ffb43a1e2ece0a74a2801d4738c153fd
AnugyaBajpai2454 <98964990+ASHUTOSH2224@users.noreply.github.com>
2023-10-08 02:49:55 +0530

    Breakpoints

--- assests/main.js
+++ assests/main.js
@@ -21,3 +21,10 @@ const linkAction= () =>{
 }
 navLink.forEach(n=>n.addEventListener('click',linkAction))
 
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')             
+}
+window.addEventListener('scroll', bgHeader)
+
